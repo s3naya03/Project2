89c00afdb49a5b17b05bef0923a01712adcaab24
s3naya03 <105098518@student.swin.edu.au>
2025-05-23 09:05:08 +1000

    All the php files were included by incs

--- search.php
+++ search.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title> jobLock | Find a job</title>
-    <link rel="stylesheet" href="styles/styles.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-  </head>
-  <body>
-    <header class="container">
-      <section class="logo">
-        <h1><a href="index.html">!</a>job<span>Lock</span></h1>
-        <p>Lock Your Dream Job</p>
-      </section>
-      <nav>
-        <ul class="nav-menu">
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="search.html">Search Jobs</a></li>
-          <li><a href="about.html">About Us</a></li>
-          <li><a href="enhancements.html">Enhancements</a></li>
-        </ul>
-      </nav>
-    </header>
+<?php include("header.inc"); ?>
+<?php include("nav.inc"); ?>
 
     <main class="job-container">
       <!-- Job 1 -->
@@ -31,7 +7,7 @@
         <h2 class="job-title">Cybersecurity Specialist</h2>
         <p class="job-company">SecureTech Solutions</p>
         <address class="job-location">Sydney, Australia</address>
-        <a href="jobs.html" class="apply-btn">See More</a>
+        <a href="jobs.php" class="apply-btn">See More</a>
       </article>
 
       <!-- Job 2 -->
@@ -39,7 +15,7 @@
         <h2 class="job-title">Data Analyst</h2>
         <p class="job-company">DataInsight Solutions</p>
         <address class="job-location">Melbourne, Australia</address>
-        <a href="choicejob.html" class="apply-btn">See More</a>
+        <a href="choicejob.php" class="apply-btn">See More</a>
       </article>
 
       <!-- Job 3 -->
@@ -94,26 +70,4 @@
       </article>
     </main>
 
-    <footer>
-      <footer>
-        <section class="container">
-          <section class="footer-content">
-            <nav class="social-icons" aria-label="Social media links">
-              <a href="mailto:105098518@student.swin.edu.au"><i class="fas fa-envelope"></i></i
-              ></a>
-              <a href="#" aria-label="Twitter"
-                ><i class="fab fa-twitter" ></i
-              ></a>
-              <a href="#" aria-label="LinkedIn"
-                ><i class="fab fa-linkedin" ></i
-              ></a>
-              <a href="#" aria-label="Instagram"
-                ><i class="fab fa-instagram" ></i
-              ></a>
-            </nav>
-            <p>&copy; 2023 jobLock. All rights reserved.</p>
-          </section>
-        </section>
-      </footer>
-  </body>
-</html>
+    <?php include("footer.inc"); ?>
